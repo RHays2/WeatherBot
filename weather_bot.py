dc0e0e99794558b5de51ff7d9444a64a9c87923c
Kevin Mattappally <kevinmat@outlook.com>
2020-02-20 00:02:16 -0800

    removal of extras

--- weather_bot.py
+++ weather_bot.py
@@ -3,13 +3,6 @@ from time import sleep
 from random import choice
 sense = SenseHat()
 
-r = 75
-g = 150
-b = 130
-blue = [0,0,255]
-sense.clear((r,g,b))
-#sense.show_message("Swag", 0.1, [r,g,b])
-
 R = (255, 50, 19)
 O = (255, 151, 28)
 Y = (255, 213, 0)
@@ -31,42 +24,3 @@ sunny  = [
 ]
 
 sense.set_pixels(sunny)
-'''
-while True:
-    sense.set_rotation(0)
-    sleep(0.5)
-    sense.set_rotation(90)
-    sleep(0.5)
-    sense.set_rotation(180)
-    sleep(0.5)
-    sense.set_rotation(270)
-    sleep(0.5)
-
-
-print "Pressure: ",sense.get_pressure()
-print "Temperature: ", sense.get_temperature()
-print "Humidity: ", sense.get_humidity()
-
-o = sense.get_orientation()
-pitch = o["pitch"]
-roll = o["roll"]
-yaw =o["yaw"]
-print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
-
-red = (255, 0, 0)
-
-while True:
-    acceleration = sense.get_accelerometer_raw()
-    x = acceleration['x']
-    y = acceleration['y']
-    z = acceleration['z']
-
-    x = abs(x)
-    y = abs(y)
-    z = abs(z)
-
-    if x > 1 or y > 1 or z > 1:
-        sense.show_letter("!", red)
-    else:
-        sense.clear()
-'''        
