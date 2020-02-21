fe9b6e8b1591d0b01a81ce7940ff18a6d4a606d1
Kevin Mattappally <kevinmat@outlook.com>
2020-02-20 16:59:08 -0800

    updating remote with weather icons

--- weather_bot.py
+++ weather_bot.py
@@ -11,16 +11,53 @@ B = (3, 65, 174)
 P = (128, 0, 128)
 N = (123, 200, 45)
 W = (255, 255, 255)
+GR = (128, 128, 128)
 
 sunny  = [
     B, B, B, B, B, B, B, B,
-    B, B, B, B, B, B, B, B,
+    B, B, B, Y, Y, B, B, B,
     B, B, Y, Y, Y, Y, B, B,
+    B, Y, Y, Y, Y, Y, Y, B,
+    B, Y, Y, Y, Y, Y, Y, B,
     B, B, Y, Y, Y, Y, B, B,
-    B, B, Y, Y, Y, Y, B, B,
-    B, B, Y, Y, Y, Y, B, B,
-    B, B, B, B, B, B, B, B,
+    B, B, B, Y, Y, B, B, B,
     B, B, B, B, B, B, B, B
 ]
 
-sense.set_pixels(sunny)
+cloudy  = [
+    B, B, B, B, B, B, B, B,
+    B, B, GR, GR, GR, GR, B, B,
+    B, GR, GR, GR, GR, GR, GR, B,
+    GR, GR, GR, GR, GR, GR, GR, GR,
+    GR, GR, GR, GR, GR, GR, GR, GR,
+    B, GR, GR, GR, GR, GR, GR, B,
+    B, B, GR, GR, GR, GR, B, B,
+    B, B, B, B, B, B, B, B
+]
+
+rainy = [
+    GR, GR, GR, B, B, GR, GR, GR,
+    GR, GR, B, B, B, B, GR, GR,
+    GR, GR, B, B, B, B, GR, GR,
+    GR, B, B, B, B, B, B, GR,
+    GR, B, B, B, B, B, B, GR,
+    GR, B, B, B, B, B, B, GR,
+    GR, GR, B, B, B, B, GR, GR,
+    GR, GR, GR, GR, GR, GR, GR, GR
+]
+
+snowy  = [
+    W, B, B, W, W, B, B, W,
+    B, W, B, W, W, B, W, B,
+    B, B, W, W, W, W, B, B,
+    W, W, W, W, W, W, W, W,
+    W, W, W, W, W, W, W, W,
+    B, B, W, W, W, W, B, B,
+    B, W, B, W, W, B, W, B,
+    W, B, B, W, W, B, B, W
+]
+
+
+
+#sense.set_pixels(sunny)
+sense.set_pixels(snowy)
